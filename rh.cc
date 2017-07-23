3ef660b99d83b0c73a2bd8cec16f4fa960a17071
maekawataiki <maekawataiki@yahoo.co.jp>
2017-07-23 14:57:55 -0400

    source

--- rh.cc
+++ rh.cc
@@ -4,14 +4,14 @@ RH::RH(): Potion{} {}
 
 void RH::defaultUsed(Player &p){
   int &hp = p.getHp();
-  hp = std::max(hp+10, p.getDfHp());
+  hp = std::min(hp+10, p.getDfHp());
   isUsed = true;
   Object::message += "PC uses RH.";
 }
 
 void RH::usedImpl(Drow &p){
   int &hp = p.getHp();
-  hp = std::max(hp+15, p.getDfHp());
+  hp = std::min(hp+15, p.getDfHp());
   isUsed = true;
   Object::message += "PC uses RH.";
 }
